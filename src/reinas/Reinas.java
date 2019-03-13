bbab1c3dea98b2b7770294baf95e2ce273cf59fe
Rodrigo Andres Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2019-03-12 19:06:05 -0500

    Project of queen made in java

---
+++ src/reinas/Reinas.java
@@ -0,0 +1,294 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reinas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andres
+ */
+public class Reinas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //Tablero 
+        int[][] tablero = {
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        //Pedir al usuario la cordenadas
+        System.out.println("Ingrese cordena X ( de 1 a 8): ");
+        Scanner entrada = new Scanner(System.in);
+        int x = entrada.nextInt();
+        int x_incial = clonarNumero(x);
+
+        System.out.println("Ingrese cordena y ( de 1 a 8): ");
+        Scanner entrada1 = new Scanner(System.in);
+        int y = entrada1.nextInt();
+        int y_inicial = clonarNumero(y);
+
+        //Se ubica la Reina
+        if (((x > 0) && (x <= 8)) && ((y > 0) && (y <= 8))) {
+            tablero[x - 1][y - 1] = 0;
+            int[] soluciones = {0, 0, 0, 0, 0, 0, 0, 0};
+            mostrarTablero(tablero);
+            System.out.println("");
+
+            resolve(tablero, x, y, soluciones, 0, 0, x_incial, y_inicial);
+
+        } else {
+            System.err.print("Cordenadas invalidas");
+        }
+
+    }
+
+    private static void resolve(int[][] tablero, int x, int y, int[] soluciones, int direccion, int suma, int x_incial, int y_inicial) {
+
+        switch (direccion) {
+            //Recorrer hacia la derecha
+            case 0:
+                if (y < tablero.length - 1) {
+
+                    y += 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[0] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+
+                break;
+
+            //Recorrer diagonal derecha - abajo-
+            case 1:
+                if ((x < tablero.length - 1) && (y < tablero.length - 1)) {
+
+                    x += 1;
+                    y += 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[1] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+
+                break;
+
+            //Recorrer abajo-
+            case 2:
+                if (x < tablero.length - 1) {
+
+                    x += 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[2] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+
+                break;
+                
+            //Recorrer abajo - izquierda
+            case 3:
+                if ((x < tablero.length - 1 ) && (y > 1 )) {
+
+                    x -= 1;
+                    y -= 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[3] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+                break;
+                
+            //Recorrer hacia la izquierda
+            case 4:
+                if (y > 1) {
+
+                    y -= 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[4] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+
+                break;
+                
+                
+            //Recorrer diagonal izquierda - arriba
+            case 5:
+                if ((x > 1) && (y > 1)) {
+
+                    x -= 1;
+                    y -= 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[5] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+
+                break;
+                
+            //Recorrer arriba
+            case 6:
+                if (x > 1) {
+
+                    x -= 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[6] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+
+                break;
+                
+            //Recorrer abajo - izquierda
+            case 7:
+                if ((x > 1 ) && (y < tablero.length -1 )) {
+
+                    x -= 1;
+                    y += 1;
+                    suma += 1;
+
+                    resolve(tablero, x, y, soluciones, direccion, suma, x_incial, y_inicial);
+
+                } else {
+                    soluciones[7] = suma;
+                    suma = 0;
+                    direccion += 1;
+                    resolve(tablero, x_incial, y_inicial, soluciones, direccion, suma, x_incial, y_inicial);
+                }
+                break;
+
+            default:
+                mostrarSoluciones(soluciones);
+                break;
+        }
+    }
+
+    private static void mostrarTablero(int[][] tablero) {
+
+        for (int i = 0; i < tablero.length - 1; i++) {
+            for (int j = 0; j < tablero.length - 1; j++) {
+                System.out.print(" " + tablero[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void mostrarSoluciones(int[] soluciones) {
+
+        for (int i = 0; i < soluciones.length; i++) {
+                       
+            switch(i){
+                
+                case 0:
+                    
+                    System.out.println("Hacia derecha la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                
+                case 1:
+                    
+                    System.out.println("Hacia derecha - abajo la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                    
+                case 2:
+                    
+                    System.out.println("Hacia abajo la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                    
+                case 3:
+                    
+                    System.out.println("Hacia abajo a la izquierda la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                    
+                case 4:
+                    
+                    System.out.println("Hacia izquierda la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                    
+                case 5:
+                    
+                    System.out.println("Hacia arriba a la izquierda la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                
+                case 6:
+                    
+                    System.out.println("Hacia arriba la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                
+                case 7:
+                    
+                    System.out.println("Hacia arriba a la derecha la cantidad de movimientos es: " + soluciones[i]);
+                    
+                    break;
+                    
+                default:
+                    System.out.println("Dirrecion incorrecta");
+                    break;
+            }
+            
+        }
+    }
+
+    private static int clonarNumero(int x) {
+        int r = x;
+        return r;
+    }
+
+}
